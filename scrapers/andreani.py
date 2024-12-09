d0f3fa8f46f1b6cbe7622534fb7f4b48fae09e41
Botato300 <bigvotati@gmail.com>
2024-12-09 12:14:24 -0300

    crea el archivo scraper para el provider 'Andreani'

--- scrapers/andreani.py
+++ scrapers/andreani.py
@@ -1,5 +1,22 @@
-from ._interface import IScraper
+import requests
+
+from .IScraper import IScraper
 
 class Andreani(IScraper):
-    def get_state(self):
-        pass
+    __API_URL = "https://tracking-api.andreani.com/api/v1/Tracking"
+
+    def get_state(self, order_id):
+        payload = {
+            "idReceptor": 1,
+            "idSistema": 1,
+            "userData": '{"mail":""}',
+            "numeroAndreani": str(order_id),
+        }
+
+        headers = {
+            "Authorization": "XqPMiwXzTRKHH0mF3gmtPtQt3LNGIuqCTdgaUHINMdmlaFid0x9MzlYTKXPxluYQ",
+        }
+
+        response = requests.get(self.__API_URL, params=payload, headers=headers, timeout=60)
+
+        return response.json()
